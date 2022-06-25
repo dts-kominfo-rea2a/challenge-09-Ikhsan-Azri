fa846717553b292c4eb0e4b5aa9e83826e61ca79
Ikhsan Azri <ikhsanazri99@gmail.com>
2022-06-26 01:18:57 +0700

    hmmmm

--- src/App.js
+++ src/App.js
@@ -8,20 +8,15 @@ import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
-  const arrContact = contacts.map((e,i)=>{
-    return <Contact data={e} key={i}/>;
-  });
-  
   return (
     <div className="App">
       <Header/>
-      {/* <Contact data={contacts[0]}/> */}
-      {/* <Contact data={contacts[1]}/>
+      <Contact data={contacts[0]}/>
+      <Contact data={contacts[1]}/>
       <Contact data={contacts[2]}/>
       <Contact data={contacts[3]}/>
       <Contact data={contacts[4]}/>
-      <Contact data={contacts[5]}/> */}
-      {arrContact}
+      <Contact data={contacts[5]}/>
     </div>
   );
 
